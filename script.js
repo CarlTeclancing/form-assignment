400dbfa6757bb725dc24ad70f73ddaa8bb836070
YuvCarl <carldrake969@gmail.com>
2022-10-14 12:42:03 +0100

    commited some changes

--- script.js
+++ script.js
@@ -7,27 +7,35 @@ const pass2 = document.getElementById('password2').value;
 let btn = document.getElementById('btn');
 
 
-function checkName(){
 
-    if(fname.length < 3 || sname.length < 3){
+btn.addEventListener("click", function(){
+    checkName();
+    checkPass;
+});
+
+function checkName(){
+    const fn = fname.length;
+    const sn = sname.length;
+    if(fn < 3 || sn < 3){
         alert("name should not be less than 3");
     }
-    else{
+    else if(fname.length > 3 || sname.length > 3){
         fname.value = "";
+        sname.value = "";
     
+    }
+    else{
+        alert("name can not be empty")
     }
 
 }
 
 function checkPass(){
-    if(pass == pass2){
+    if(pass === pass2){
+        alert("okay pass")
         pass  = "";
         pass2 = "";
     }else{
         alert("passwords do not match")
     }
 }
-btn.addEventListener("click", function(){
-    checkName();
-    checkPass();
-})
