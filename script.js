6448029e20004038a7260fcfbe023a2a439bcc01
YuvCarl <carldrake969@gmail.com>
2022-10-14 15:23:44 +0100

    commited some changes

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-const fname = document.getElementById('fname').value;
-const sname = document.getElementById('sname').value;
-const email = document.getElementById('email').value;
-const pass = document.getElementById('password').value;
-const pass2 = document.getElementById('password2').value;
+const fname = document.getElementById('fname');
+const sname = document.getElementById('sname');
+const email = document.getElementById('email');
+const pass = document.getElementById('password');
+const pass2 = document.getElementById('password2');
 
 let btn = document.getElementById('btn');
 
 
 
 btn.addEventListener("click", function(){
-    checkName();
-    checkPass;
+    //checkName();
+    checkPass();
 });
 
 function checkName(){
-    const fn = fname.length;
-    const sn = sname.length;
-    if(fn < 3 || sn < 3){
+    const fn = fname.value.length;
+    const sn = sname.value.length;
+    if((fn < 3) || (sn < 3)){
         alert("name should not be less than 3");
     }
-    else if(fname.length > 3 || sname.length > 3){
+    else if((fn > 3) || (sn > 3)){
         fname.value = "";
         sname.value = "";
     
@@ -31,11 +31,16 @@ function checkName(){
 }
 
 function checkPass(){
-    if(pass === pass2){
-        alert("okay pass")
+    const p1 = pass.value;
+    const p2 = pass2.value;
+    if((p1 == "") || (p2 == "")){
+        alert("password can not be empty")
         pass  = "";
         pass2 = "";
-    }else{
+    }else if(p1 == p2){
+        alert("pasword sucesfully")
+    }
+    else{
         alert("passwords do not match")
     }
 }
